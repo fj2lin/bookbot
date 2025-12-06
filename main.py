ddeabc6a4bdcf8ea8b42468fcbbdb0e537829379
fj2lin <63832778+fj2lin@users.noreply.github.com>
2025-12-05 23:19:16 -0300

    created stats

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from stats import get_num_words
+
 def get_book_text(path):
     with open(path) as f:
         text = f.read()
     return text
 
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    num_words = get_num_words(get_book_text("books/frankenstein.txt"))
+    print(f"Found {num_words} total words")
 
 main()
