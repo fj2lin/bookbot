1123cf0b5008a5ade165606669a9b1b3895c7c9d
fj2lin <63832778+fj2lin@users.noreply.github.com>
2025-12-05 23:01:51 -0300

    can open books now

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(path):
+    with open(path) as f:
+        text = f.read()
+    return text
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))
+
+main()
